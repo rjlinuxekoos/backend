7f2e15d1bac64ef8283c4ed54d20d1d74be1d86f
Rider Nunes <rjlinux@gmail.com>
2019-04-13 19:00:59 -0300

    Adiciona16

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ app.use((req,res, next) => {
     req.io = io;
     return next();
 }
-)
+);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));//permite enviar arquivos nas comunicacoes
